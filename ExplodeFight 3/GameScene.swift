78e179a64ef0d030e3392560958c584a5e46ff6f
rich@anubis <r.j.henry@gmail.com>
2020-12-01 10:09:48 +0000

    Inception Touch controls and window function. More to come but this can probably progress with the game.

--- ExplodeFight 3/GameScene.swift
+++ ExplodeFight 3/GameScene.swift
@@ -8,7 +8,6 @@
 
 import SpriteKit
 import SpriteKitAddons
-import GameplayKit
 import GameControls
 
 class GameScene: BaseSKScene, ButtonSKSpriteNodeResponder {
@@ -20,12 +19,29 @@ class GameScene: BaseSKScene, ButtonSKSpriteNodeResponder {
         guard let moveIndicator = self.childNode(withName: "//joystickBoundsLeft/indicator") else { return }
         guard let fireIndicator = self.childNode(withName: "//joystickBoundsRight/indicator") else { return }
         
-        let absoluteFunctions: [TouchFunction] = [
+        let absoluteFunctions: [TouchJoystick.TouchFunction] = [
             { touch in moveIndicator.position = touch.location(in: self) },
             { touch in fireIndicator.position = touch.location(in: self) }
         ]
         
+        let moveWindowFunc = TouchJoystick.WindowFunction(windowSize: CGSize(width: 80, height: 80), deadZoneR2: 10)
+        let fireWindowFunc = TouchJoystick.WindowFunction(windowSize: CGSize(width: 80, height: 80))
+
+        let windowFunctions: [TouchJoystick.TouchFunction] = [
+            { touch in
+                
+                moveWindowFunc.handleTouch(touch: touch)
+                moveIndicator.position = moveWindowFunc.windowVector
+            },
+            { touch in
+                
+                fireWindowFunc.handleTouch(touch: touch)
+                fireIndicator.position = fireWindowFunc.windowVector
+            }
+        ]
+
         joystick.joyFunctions = absoluteFunctions
+        joystick.joyFunctions = windowFunctions
     }
     
     override func update(delta: TimeInterval) {
@@ -43,33 +59,13 @@ class GameScene: BaseSKScene, ButtonSKSpriteNodeResponder {
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        joystick.touchesBegan(touches, with: event)
-        //for (i, t) in touches.enumerated() { printToLabel("Begin touch \(i) at \(t.location(in: self))") }
-        //for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) { joystick.touchesBegan(touches, with: event) }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        joystick.touchesMoved(touches, with: event)
-        //for (i, t) in touches.enumerated() { printToLabel("Moved touch \(i) at \(t.location(in: self))") }
-        //for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) { joystick.touchesMoved(touches, with: event) }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        joystick.touchesEnded(touches, with: event)
-        //for (i, t) in touches.enumerated() { printToLabel("Ended touch \(i) at \(t.location(in: self))") }
-        //for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) { joystick.touchesEnded(touches, with: event) }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        joystick.touchesCancelled(touches, with: event)
-        //for (i, t) in touches.enumerated() { printToLabel("Cancelled touch \(i) at \(t.location(in: self))") }
-        //for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) { joystick.touchesCancelled(touches, with: event) }
     
     func buttonTriggered(button: ButtonSKSpriteNode) {
         
@@ -85,208 +81,3 @@ class GameScene: BaseSKScene, ButtonSKSpriteNodeResponder {
         }
     }
 }
-
-class TouchJoystick {
-    
-    typealias JoystickTouchFunction = (_ touch: UITouch) -> Void
-    
-    public var joyFunctions: [TouchFunction] = []
-
-    private var joyTouches: [UITouch?] = []
-
-    func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        touches.forEach { t in
-            
-            // Use the first slot in the array where we find a nil value. If there are no nil slots, append to the array
-            // as long as there aren't more touches than there are functions to be called on that touch.
-            if let i = joyTouches.firstIndex(where: { $0 == nil }) {
-                
-                joyTouches[i] = t
-                joyFunctions[i](t)
-                
-            } else {
-                
-                if joyTouches.count < joyFunctions.count {
-                    
-                    joyFunctions[joyTouches.count](t)
-                    joyTouches.append(t)
-                }
-            }
-        }
-    }
-    
-    func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        touches.forEach { t in
-
-            // Find the touch in the touch array and call the appropriate function (at the same index).
-            if let i = joyTouches.firstIndex(where: { $0 == t }) { joyFunctions[i](t) }
-        }
-    }
-    
-    func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        touches.forEach { t in
-
-            // Set the appropriate slot in the array to nil so that it can be re-used.
-            if let i = joyTouches.firstIndex(where: { $0 == t }) {
-                
-                joyFunctions[i](t)
-                joyTouches[i] = nil
-            }
-        }
-    }
-    
-    func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        // Just do the same operation as if the touch had ended.
-        touchesEnded(touches, with: event)
-    }
-}
-
-/*
-//  TouchController.swift
-//  GameControls
-//
-//  Created by Richard Henry on 20/03/2017.
-//  Copyright © 2017 Dogstar Industries. All rights reserved.
-//
-
-import UIKit
-
-public typealias TouchFunction = (_ touch: UITouch) -> Void
-
-public class TouchControllerView : UIView {
-    
-    @IBInspectable public var viewName: String = "Unnamed"
-    
-    public var joyFunctions: [TouchFunction] = []
-    
-    private var joyTouches: [UITouch?] = []
-    
-    required public override init(frame: CGRect) {
-        
-        super.init(frame: frame)
-        
-        #if DEBUG
-            if let grArray = self.gestureRecognizers, grArray.count > 0 {
-                
-                print("TouchStickController warning: view \(self) already has \(grArray.count) gesture recognisers.")
-                
-                for (i, gr) in grArray.enumerated() { print("Recogniser \(i): \(gr)") }
-            }
-        #endif
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        
-        super.init(coder: aDecoder)
-        
-        #if DEBUG
-            if let grArray = self.gestureRecognizers, grArray.count > 0 {
-                
-                print("TouchStickController warning: view \(self) already has \(grArray.count) gesture recognisers.")
-                
-                for (i, gr) in grArray.enumerated() { print("Recogniser \(i): \(gr)") }
-            }
-        #endif
-    }
-
-    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        for t in touches {
-            
-            // Use the first slot in the array where we find a nil value. If there are no nil slots, append to the array
-            // as long as there aren't more touches than there are functions to be called on that touch.
-            if let i = joyTouches.firstIndex(where: { $0 == nil }) {
-                
-                joyTouches[i] = t
-                joyFunctions[i](t)
-                
-            } else {
-                
-                if joyTouches.count < joyFunctions.count {
-                    
-                    joyFunctions[joyTouches.count](t)
-                    joyTouches.append(t)
-                }
-            }
-        }
-    }
-    
-    override public func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        for t in touches {
-            
-            // Find the touch in the touch array and call the appropriate function (at the same index).
-            if let i = joyTouches.firstIndex(where: { $0 == t }) { joyFunctions[i](t) }
-        }
-    }
-    
-    override public func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        for t in touches {
-            
-            // Set the appropriate slot in the array to nil so that it can be re-used.
-            if let i = joyTouches.firstIndex(where: { $0 == t }) {
-                
-                joyFunctions[i](t)
-                joyTouches[i] = nil
-            }
-        }
-    }
-    
-    override public func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        // Just do the same operation as if the touch had ended.
-        touchesEnded(touches, with: event)
-    }
-}
-
-public class WindowFunction {
-    
-    public var windowVector: CGPoint = CGPoint()
-    public var deadZoneRadiusSquared: CGFloat = 0.0
-    
-    private let size:  CGSize
-    private var origin: CGPoint = CGPoint()
-    
-    public init(windowSize size: CGSize, deadZoneR2 dzr2: CGFloat = 0.0) {
-        
-        self.size = size
-        self.deadZoneRadiusSquared = dzr2 * dzr2
-    }
-    
-    public func handleTouch(touch: UITouch) {
-        
-        let touchPoint = touch.location(in: touch.view)
-        
-        switch touch.phase {
-            
-        case .began:
-            origin = touchPoint
-            windowVector = CGPoint()
-            
-        case .moved:
-            var vector = CGPoint(x:  touchPoint.x - origin.x, y: -(touchPoint.y - origin.y))
-            
-            // Clip that vector
-            if vector.x > size.width { origin.x += vector.x - size.width; vector.x = size.width }
-            else if vector.x < -size.width { origin.x += vector.x + size.width; vector.x = -size.width }
-            
-            if vector.y > size.height { origin.y -= vector.y - size.height; vector.y = size.height }
-            else if vector.y < -size.height { origin.y -= vector.y + size.height; vector.y = -size.height }
-            
-            let vectorLengthSquared = vector.x * vector.x + vector.y * vector.y
-            
-            windowVector = vectorLengthSquared > deadZoneRadiusSquared ? vector : CGPoint()
-            
-        case .ended, .cancelled:
-            windowVector = CGPoint()
-
-        default:break
-        }
-    }
-}
-*/
