beb60596e821a4b0a9696fe11f4a92792e10ba2a
rich@basilisk <r.j.henry@gmail.com>
2020-12-12 17:26:38 +0000

    Begin with the rules and whatnot

---
+++ ExplodeFight 3/RobotRules.swift
@@ -0,0 +1,9 @@
+//
+//  RobotRules.swift
+//  ExplodeFight 3
+//
+//  Created by Richard Henry on 12/12/2020.
+//  Copyright © 2020 Dogstar Industries Ltd. All rights reserved.
+//
+
+import Foundation
