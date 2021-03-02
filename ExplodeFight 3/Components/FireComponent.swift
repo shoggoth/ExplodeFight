47ea14ca0e9ef81e76d9b4c62e635b121919a1ff
rich@anubis <r.j.henry@gmail.com>
2021-03-02 11:00:28 +0000

    Moved in Weapon initial bits from exp/shoot

--- ExplodeFight 3/Components/FireComponent.swift
+++ ExplodeFight 3/Components/FireComponent.swift
@@ -1,8 +1,8 @@
 //
 //  FireComponent.swift
-//  EF3
+//  ExplodeFight 3
 //
-//  Created by Richard Henry on 03/02/2021.
+//  Created by Richard Henry on 08/01/2021.
 //  Copyright © 2021 Dogstar Industries Ltd. All rights reserved.
 //
 
@@ -11,8 +11,59 @@ import SpriteKitAddons
 
 class FireComponent: GKComponent {
     
+    @GKInspectable var weaponType = 1
+    
+    private var fireTicker = PeriodicTimer(tickInterval: 1.0)
+    private var weapon: Weapon? = nil
+    
+    override func didAddToEntity() {
+        
+        print("Weapon type = \(weaponType)")
+        
+        switch weaponType {
+        case 1:
+            weapon = {
+                let w = NodeCannon()
+                w.emitNode = entity?.spriteComponent?.node
+                w.magazine = [1, 1, 1].map { rad in  RoundBullet(radius: rad) }
+
+                return w
+            }()
+        case 2:
+            weapon = {
+                let w = NodeCannon()
+                w.emitNode = entity?.spriteComponent?.node
+                w.magazine = [1, 2, 3, 4, 5, 4, 3, 2, 1].map { rad in  RoundBullet(radius: rad) }
+
+                return w
+            }()
+        case 3:
+            weapon = {
+                let w = NodeCannon()
+                w.emitNode = entity?.spriteComponent?.node
+                w.magazine = [10, 10, 10].map { rad in  RoundBullet(radius: rad) }
+
+                return w
+            }()
+        case 4:
+            weapon = {
+                let w = NodeCannon()
+                w.emitNode = entity?.spriteComponent?.node
+                w.magazine = [2, 16, 32].map { rad in  RoundBullet(radius: rad) }
+
+                return w
+            }()
+        default: break
+        }
+    }
+    
     override func update(deltaTime seconds: TimeInterval) {
         
-        super.update(deltaTime: seconds)
+        guard let weapon = weapon, let sourceNode = entity?.spriteComponent?.node else { return }
+        
+        fireTicker = fireTicker.tick(deltaTime: seconds) { weapon.fire(direction: CGVector(angle: sourceNode.zRotation + (pi * 0.5))) }
     }
+    
+    override class var supportsSecureCoding: Bool { return true }
 }
+
---
+++ ExplodeFight 3/Weapon.swift
@@ -0,0 +1,112 @@
+//
+//  Weapon.swift
+//  ExplodeFight 3
+//
+//  Created by Richard Henry on 12/01/2021.
+//  Copyright © 2021 Dogstar Industries Ltd. All rights reserved.
+//
+
+import GameplayKit
+import SpriteKitAddons
+
+protocol Weapon {
+    
+    func fire(direction: CGVector)
+}
+
+protocol NodeBullet: SKNode {
+    
+    func fire(completion: ((NodeBullet) -> Void)?)
+    func reset()
+}
+
+// MARK: - Weapons -
+
+class NodeCannon: Weapon {
+    
+    var magazine: [NodeBullet]?
+    var emitNode: SKNode?
+
+    func fire(direction: CGVector) {
+        
+        guard let emitNode = self.emitNode,
+              let scene = emitNode.scene,
+              let bullet = magazine?.popLast() else { return }
+        
+        //bullet.physicsBody = nil
+        bullet.physicsBody?.velocity = direction * 1024
+        
+        let firePos = CGPoint(x: 0, y: 34)
+        bullet.position = scene.convert(firePos, from: emitNode)
+        
+        scene.addChild(bullet)
+
+        bullet.fire { b in
+            
+            b.reset()
+            self.magazine?.insert(b, at: 0)
+        }
+    }
+}
+
+// MARK: - Bullets -
+
+class RoundBullet: SKShapeNode, NodeBullet {
+    
+    override init() {
+        
+        super.init()
+        
+        name = "Round Bullet"
+        setup(radius: 4)
+    }
+    
+    init(radius: CGFloat) {
+        
+        super.init()
+        
+        setup(radius: radius)
+    }
+    
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    func setup(radius: CGFloat) {
+        
+        let diameter = radius * 2
+        
+        path = CGPath(ellipseIn: CGRect(origin: CGPoint(x: -radius, y: -radius), size: CGSize(width: diameter, height: diameter)), transform: nil)
+        lineWidth = radius * 0.25
+        //glowWidth = radius * 0.125
+        strokeColor = .blue
+        
+        // TODO: Need physics for collision?
+        physicsBody = SKPhysicsBody(circleOfRadius: radius)
+    }
+    
+    func fire(completion: ((NodeBullet) -> Void)? = nil) {
+
+        // TODO: Use an action for the movement here abd allow the fire method to be switched between that and one propelled by physics velocity.
+        //run(SKAction.playSoundFileNamed("blast.caf", waitForCompletion: false))
+        run(SKAction.repeatForever(SKAction.rotate(byAngle: pi, duration: 1)))
+        run(SKAction.sequence([SKAction.wait(forDuration: 0.75), SKAction.fadeOut(withDuration: 0.25), SKAction.removeFromParent(), SKAction.run { self.reset(); completion?(self) }]))
+    }
+    
+    func reset() {
+        
+        removeAllActions()
+        
+        alpha = 1
+    }
+}
+
+// MARK: -
+
+class DebugBullet: RoundBullet {
+    
+    override init() { super.init() }
+    
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    deinit { print("deinit") }
+}
+
