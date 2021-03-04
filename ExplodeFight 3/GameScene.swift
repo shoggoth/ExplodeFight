e79bae715f4a5571fcbe95d4a1467337c6bec475
rich@anubis <r.j.henry@gmail.com>
2021-03-04 11:30:52 +0000

    Mapping working with single character strings for keys but I need some multi character keys.

--- ExplodeFight 3/GameScene.swift
+++ ExplodeFight 3/GameScene.swift
@@ -38,34 +38,45 @@ class GameScene: BaseSKScene {
     
     private func tilemapbits() {
         
-        //guard let edNode = scene?.childNode(withName: "CharacterTileMap") as? SKTileMapNode else { return }
+        guard let edNode = scene?.childNode(withName: "CharacterTileMap") as? SKTileMapNode else { return }
         guard let prNode = scene?.childNode(withName: "ProgrTMRoot") else { return }
         
+        // Shader?
+        edNode.shader = SKShader(fileNamed: "charMap.fsh")
+
         makeTileMap(on: prNode)
     }
     
     private func makeTileMap(on node: SKNode) {
         
-        print("making programmatic tile map... \(node)")
-        
-        // Create tile sets
-        let groups = "A6720".map { SKTileGroup(tileDefinition: SKTileDefinition(texture: SKTexture(imageNamed: String($0)))) }
-        let tileSet = SKTileSet(tileGroups: groups)
-        tileSet.defaultTileSize = CGSize(width: 32, height: 32)
-        
-        // Create tile map
-        let tileMap = SKTileMapNode(tileSet: tileSet, columns: 32, rows: 4, tileSize: tileSet.defaultTileSize)
-        tileMap.fill(with: groups.randomElement())
-        tileMap.setTileGroup(groups.first, forColumn: 1, row: 1)
-        tileMap.setTileGroup(groups.first, forColumn: 0, row: 2)
-        tileMap.setTileGroup(groups.first, forColumn: 1, row: 3)
+        let mapper = CharTileSetMapper(alphabet: "ABCDW6720", defaultSize: CGSize(width: 32, height: 32))
 
-        // Shader?
-        tileMap.shader = SKShader(fileNamed: "charMap.fsh")
+        // Create tile map
+        let tileMap = SKTileMapNode(tileSet: mapper.tileSet, columns: 32, rows: 4, tileSize: mapper.tileSet.defaultTileSize)
+        tileMap.fill(with: mapper.tiles["W"])
+        tileMap.setTileGroup(mapper.tiles["A"], forColumn: 1, row: 1)
+        tileMap.setTileGroup(mapper.tiles["B"], forColumn: 0, row: 2)
+        tileMap.setTileGroup(mapper.tiles["C"], forColumn: 1, row: 3)
         
         // Add it to the scene
         node.addChild(tileMap)
-        tileMap.run(SKAction.sequence([SKAction.wait(forDuration: 3.0), SKAction.run { tileMap.setTileGroup(groups.first, forColumn: 3, row: 3) }]))
+        tileMap.run(SKAction.sequence([SKAction.wait(forDuration: 3.0),
+                                       SKAction.run { tileMap.setTileGroup(mapper.tiles["D"], forColumn: 3, row: 3) },
+                                       SKAction.wait(forDuration: 1.0),
+                                       SKAction(named: "TMAnimation")!]))
+    }
+}
+
+struct CharTileSetMapper {
+    
+    let tiles: [String : SKTileGroup]
+    let tileSet: SKTileSet
+
+    init(alphabet: String, defaultSize: CGSize) {
+        
+        tiles = Dictionary(uniqueKeysWithValues: alphabet.map { (String($0), SKTileGroup(tileDefinition: SKTileDefinition(texture: SKTexture(imageNamed: String($0))))) })
+        tileSet = SKTileSet(tileGroups: tiles.map { $0.1 })
+        tileSet.defaultTileSize = defaultSize
     }
 }
 
