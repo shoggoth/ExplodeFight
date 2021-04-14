0275788ac9a8463474da0fa8a7e3ff70d84a2132
rich@anubis <r.j.henry@gmail.com>
2021-04-14 09:29:28 +0100

    Set up for movement development

--- ExplodeFight 3/Level.swift
+++ ExplodeFight 3/Level.swift
@@ -12,11 +12,6 @@ import SpriteKitAddons
 class Level {
     
     private let scene: GameScene
-    private let mobSpawner: SceneSpawner
-    private let explodeShader = ExplodeShader(shaderName: "explode.fsh")
-    private let ruleSystem: GKRuleSystem = GKRuleSystem()
-
-    private var spawnTicker: PeriodicTimer? = PeriodicTimer(tickInterval: 0.7)
 
     init(scene: GameScene) {
         
@@ -25,13 +20,6 @@ class Level {
         // Global setup
         AppDelegate.soundManager.playNode = scene
         
-        // Setup spawner
-        mobSpawner = SceneSpawner(scene: SKScene(fileNamed: "Mobs")!)
-        
-        // Setup level rules
-        ruleSystem.add(GKRule(predicate: NSPredicate(format: "$mobCount.intValue < 10"), assertingFact: "mobCountIsLow" as NSObject, grade: 1.0))
-        ruleSystem.add(GKRule(predicate: NSPredicate(format: "$mobCount.intValue == 0"), assertingFact: "allMobsDestroyed" as NSObject, grade: 1.0))
-
         // Setup Player
         if let node = scene.childNode(withName: "Player") {
             
@@ -44,60 +32,5 @@ class Level {
     }
     
     func update(deltaTime: TimeInterval) {
-
-        mobSpawner.update(deltaTime: deltaTime)
-        
-        ruleSystem.reset()
-        ruleSystem.state["mobCount"] = mobSpawner.activeCount
-        ruleSystem.evaluate()
-        
-        // TODO: Move this elsewhere
-        if ruleSystem.grade(forFact: "mobCountIsLow" as NSObject) >= 1.0 {
-                        
-            spawnTicker = spawnTicker?.tick(deltaTime: deltaTime) { spawnTemp() }
-        }
-    }
-    
-    func spawnTemp() {
-        
-        let mobName = "Ship"
-        
-        let _ = mobSpawner.spawn(name: mobName) { node in
-            
-            let mobEntity = MobEntity(withNode: node)
-            
-            mobEntity.addComponent(MobComponent(states: [
-                                                    LiveState {
-                                                        if let node = node as? SKSpriteNode {
-                                                            
-                                                            node.position = CGPoint.zero
-                                                            node.isPaused = false
-                                                            node.shader = nil
-                                                            node.xScale = 1.0
-                                                            node.yScale = 1.0
-                                                        }
-                                                        
-                                                        return CountdownTimer(countDownTime: 3.0)
-                                                    },
-                                                    ExplodeState {
-                                                        if let node = node as? SKSpriteNode {
-                                                            
-                                                            self.explodeShader.explode(node: node, toScale: vector_float2(7, 1), withSplits: vector_float2(16, 1), duration: 1)
-                                                        }
-                                                        
-                                                        //AppDelegate.soundManager.playSound(name: "Explode")
-                                                        
-                                                        return CountdownTimer(countDownTime: 1.0)
-                                                    },
-                                                    DieState {
-                                                        self.mobSpawner.spawner(named: mobName)?.kill(node: node, recycle: true)
-                                                    }]))
-            
-            //mobEntity.addComponent(DebugComponent())
-            
-            self.scene.addChild(node)
-
-            return mobEntity
-        }
     }
 }
