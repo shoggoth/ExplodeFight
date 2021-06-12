110687345380f5c4f701c30844aef3084440ef99
rich@basilisk <r.j.henry@gmail.com>
2021-06-12 11:47:50 +0100

    Linear charset is working well

--- ExplodeFight 3/ScoreFlyOff.swift
+++ ExplodeFight 3/ScoreFlyOff.swift
@@ -9,9 +9,69 @@
 import SpriteKit
 import SpriteKitAddons
 
+struct LinearTexCharSet {
+
+    let textureDict: [Character : SKTexture]
+    
+    init(texName: String, charCount: CGFloat) {
+        
+        let charTexture = SKTexture(imageNamed: texName)
+        let charWidth = 1.0 / charCount
+        textureDict = Dictionary(uniqueKeysWithValues: " ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,'?!/-".enumerated().map { (index: Int, key: Character) -> (Character, SKTexture) in
+            
+            let tex = SKTexture(rect: CGRect(x: CGFloat(index) * charWidth, y: 0, width: charWidth, height: 1), in: charTexture)
+            tex.filteringMode = .nearest
+            
+            return (key, tex)
+        })
+    }
+    
+    func makeNodeString(string: String, spacing: Int) -> SKNode {
+        
+        let rootNode = SKNode()
+
+        string.enumerated().forEach { index, char in
+            
+            let node = SKSpriteNode(texture: textureDict[char])
+            node.position = CGPoint(x: CGFloat(spacing * index), y: 0)
+            rootNode.addChild(node)
+        }
+
+        return rootNode
+    }
+}
+
+extension LinearTexCharSet {
+    
+    func flyup(string: String, spacing: Int, parentNode: SKNode) {
+        
+        let rootNode = makeNodeString(string: string, spacing: spacing)
+
+        rootNode.position = parentNode.position + CGVector(dx: 0, dy: 32)
+        rootNode.run(.sequence([.wait(forDuration: 1.0), SKAction(named: "FlyUp")!]))
+
+        parentNode.scene?.addChild(rootNode)
+    }
+}
+
 extension GameScene {
     
     func scoreflyoff() {
+        
+        guard let parentNode = scene?.childNode(withName: "ShipSprite") else { return }
+        let node = LinearTexCharSet(texName: "5x5Charset", charCount: 44).makeNodeString(string: "WELL, WHAT CAN THIS BE?", spacing: 5)
+        
+        parentNode.addChild(node)
+    }
+    
+    func scoreflyoff1() {
+        
+        guard let parentNode = scene?.childNode(withName: "ShipSprite") else { return }
+        
+        LinearTexCharSet(texName: "5x5Charset", charCount: 44).flyup(string: "HELLO", spacing: 5, parentNode: parentNode)
+    }
+    
+    func scoreflyoff2() {
         
         guard let parentNode = scene?.childNode(withName: "ShipSprite"), let scene = scene else { return }
 
